fde5c5bd32dc41634c0e29438b388f5a584e1937
Kundan Thakre <kundandev321@gmail.com>
2026-05-28 20:35:39 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,15 +1,9 @@
-import sys
-import os
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-from app import app as flask_app
+from app.app import app as flask_app
 
 def test_home():
     client = flask_app.test_client()
     response = client.get('/')
     assert response.status_code == 200
-
 
 def test_health():
     client = flask_app.test_client()
