edafcf4f9c1407af9ea049a1849a758caa12dd35
Kundan Thakre <kundandev321@gmail.com>
2026-05-28 20:31:16 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,18 +1,17 @@
 import sys
 import os
 
-# add project root to path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-import app
+from app import app
 
 def test_home():
-    client = app.app.test_client()
+    client = app.test_client()
     response = client.get('/')
     assert response.status_code == 200
 
 
 def test_health():
-    client = app.app.test_client()
+    client = app.test_client()
     response = client.get('/health')
     assert response.status_code == 200
